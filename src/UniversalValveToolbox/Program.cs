c6fc7a4d15bded19fadaa9296d1010dc656642e8
stam <stam@epicm.org>
2020-01-09 18:09:11 +0300

    codestyle edit

--- src/UniversalValveToolbox/Program.cs
+++ src/UniversalValveToolbox/Program.cs
@@ -4,16 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace UniversalValveToolbox
-{
-    static class Program
-    {
+namespace UniversalValveToolbox {
+    static class Program {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
-        {
+        static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
